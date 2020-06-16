f8cc60232e592e0c3b9af6ef79ca1483f35ce04d
ZIming Mao <ziming.mao@yale.edu>
2020-06-15 22:26:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,26 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+
+    post '/pirates' do 
+      # binding.pry
+
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      # binding.pry
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship[:name], ship[:type], ship[:booty])
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
   end
 end

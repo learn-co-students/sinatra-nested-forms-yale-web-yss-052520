353e77843a269715d6b43b56422446f499fa7689
edensongjbs <edensongjbs@gmail.com>
2020-06-16 17:55:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      erb :show
+    end
 
   end
 end

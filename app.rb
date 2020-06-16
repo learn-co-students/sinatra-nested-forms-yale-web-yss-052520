b73d4ab0b5cefbfce8c9c8fdf40d1815ad6ef3de
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-15 23:06:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do 
+      erb :root
+    end 
 
+    get '/new' do 
+      # put a full path to the view ERB file
+      erb :'pirates/new'
+    end 
+
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details[:name], details[:type], details[:booty])
+      end 
+      @ships = Ship.all 
+      erb :'pirates/show'
+    end 
   end
 end

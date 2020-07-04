c49acb4f21ed7dbad3f10863cbfd95580dcf0a96
zayzay500 <lindsay.pierce@yale.edu>
2020-07-04 17:27:07 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    
+    @@all = []
+    
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        save
+      end
+    
+    def save
+        self.class.all << self
+    end
+    
+    def self.all
+        @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    
+    @@all = []
+    
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        save
+      end
+    
+    def save
+        self.class.all << self
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.clear
+        self.all.clear
+    end
 end
